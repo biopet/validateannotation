21514f842cd0583f71c70a58520ba8b57e2e35b5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/main/scala/nl/biopet/tools/validateannotation/Args.scala
@@ -0,0 +1,5 @@
+package nl.biopet.tools.validateannotation
+
+import java.io.File
+
+case class Args(inputFile: File = null)
---
+++ src/test/scala/nl/biopet/tools/validateannotation/ValidateAnnotationTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.validateannotation
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object ValidateAnnotationTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
