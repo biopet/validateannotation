aa0d808727cddf5c0edb15030b6ed29de356d260
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/validateannotation/ValidateAnnotationTest.scala
+++ src/test/scala/nl/biopet/tools/validateannotation/ValidateAnnotationTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.validateannotation
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ValidateAnnotationTest extends BiopetTest {
+class ValidateAnnotationTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
