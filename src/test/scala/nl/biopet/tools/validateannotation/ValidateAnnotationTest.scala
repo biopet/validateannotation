0d646e965207ffc815ab1a913eb7ec7199cd9329
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/validateannotation/ValidateAnnotationTest.scala
+++ src/test/scala/nl/biopet/tools/validateannotation/ValidateAnnotationTest.scala
@@ -7,7 +7,7 @@ object ValidateAnnotationTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      ValidateAnnotation.main(Array())
     }
   }
 }
