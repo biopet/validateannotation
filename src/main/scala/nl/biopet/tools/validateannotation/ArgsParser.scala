efb7466e5b456655cc317d737941e3695f984086
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-20 12:05:01 +0100

    Create headers, scalafmt

--- src/main/scala/nl/biopet/tools/validateannotation/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/validateannotation/ArgsParser.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.validateannotation
 
 import java.io.File
@@ -17,8 +38,7 @@ class ArgsParser(toolCommand: ToolCommand[Args])
     (x, c) =>
       c.copy(reference = x)
   } text "Reference fasta to check vcf file against"
-  opt[Unit]("disableFail") maxOccurs 1 valueName "<file>" action {
-    (_, c) =>
-      c.copy(failOnError = false)
+  opt[Unit]("disableFail") maxOccurs 1 valueName "<file>" action { (_, c) =>
+    c.copy(failOnError = false)
   } text "Do not fail on error. The tool will still exit when encountering an error, but will do so with exit code 0"
 }
